16e4a0627579055df9adf0a5ecff22ad07939b89
vishwasuvarna07 <vishwatejasuvarna@gmail.com>
2020-02-05 11:07:12 -0600

    Testing Strategies class

--- Assignment1/src/RandomSearchStrategy.java
+++ Assignment1/src/RandomSearchStrategy.java
@@ -2,58 +2,11 @@
 public class RandomSearchStrategy implements SearchStrategy{
 	public void performSearch (int[][] grid){
 		
-		/*boolean[][] result = new boolean[grid.length][grid.length];		// Stores the results of the search
-		boolean[][] searchGrid = new boolean[grid.length][grid.length];		// Stores the visited cells 
-		int searchCount = 0;		// Stores the number of cells searched
-		int foundCount = 0;			// Stores the number of cells found to contain a battleship
-		// While the number of cells found is less than the total number of cells containing battleships...
-		for (int k = 0; k < grid.length; k++) 
-		{
-			for (int l = 0; l < grid.length; l++) 
-			{
-				result[k][l] = false;
-				searchGrid[k][l] = false;	
-			}
-		}
-		while (foundCount < 8)
-		{
-			// Select random cell coordinates
-			int i = rand.nextInt(25);
-			int j = rand.nextInt(25);
-			if (searchGrid[i][j] == true)			// If cell has already been searched...
-			{
-				while (searchGrid[i][j] == true)	// While we continue to choose a random cell that has already been searched...
-				{
-					// Select a new random cell
-					i = rand.nextInt(25);	
-					j = rand.nextInt(25);
-				}
-			}
-			searchCount++;
-			searchGrid[i][j] = true;				// Set this cell to 'searched'
-			if (gameGrid[i][j] == true && result[i][j] != true)		// If the cell contains a battleship and we haven't already accounted for it...
-			{
-				foundCount++;
-				result[i][j] = true;		// Account for the found cell in our return variable
-				searchGrid[i][j] = true;
-			}
-		}
-		System.out.println("Number of cells searched: " + searchCount);
-	}
-	}*/
-		
         int noOfIteratings=0;
         int count = 0;
         //String str;
         int[][] result = new int[grid.length][grid.length];
         System.out.println("Strategy: "+"Random Sweep");
-		for (int k = 0; k < grid.length; k++) 
-		{
-			for (int l = 0; l < grid.length; l++) 
-			{
-				result[k][l] = 0;
-			}
-		}
         while(count <8){
             int i= (int)(Math.random()*25);
             int j=(int)(Math.random()*25);
