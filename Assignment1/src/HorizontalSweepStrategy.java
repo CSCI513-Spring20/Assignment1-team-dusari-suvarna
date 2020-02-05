84444ad204653a5228ff3f59b5a3d03d0330ca9a
manishdusari <manishrajgouddusari@gmail.com>
2020-02-04 20:42:12 -0600

    "commit"

--- Assignment1/src/HorizontalSweepStrategy.java
+++ Assignment1/src/HorizontalSweepStrategy.java
@@ -1,4 +1,23 @@
-
 public class HorizontalSweepStrategy implements SearchStrategy{
+	
 
+	public void performSearch (int[][] grid){
+		int noOfIteratings = 0;
+		for(int i=0 ; i<25; i++) {
+			for(int j =0; j<25;j++) {
+				if (grid[i][j]!=0){
+					if(grid[i][j]==1) {
+					finalCoordinates[i][j] = 1;
+					}
+					else if(grid[i][j]==2) {
+						finalCoordinates[i][j] = 2;
+						}
+					noOfIteratings++;
+					System.out.println("Number of cells Searched:"+noOfIteratings);
+			}
+		} 
+			
+
+		}
+	}	
 }
--- Assignment1/src/SearchStrategy.java
+++ Assignment1/src/SearchStrategy.java
@@ -1,4 +1,4 @@
-
 public interface SearchStrategy {
-
+	int[][] finalCoordinates = new int[10][10];
+	public void performSearch(int[][] game);
 }
